3247ff27dafe381a9e0082a8fa58e1a4c80191aa
root <root@alex-AOD257.(none)>
2014-02-01 01:55:53 +0800

    created the basic templates and routing

---
+++ app/views/layouts/home-master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>UMAK-CCS Alumni Tracking System</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
+    </head>
+    <body>
+        <div id="page">
+            @include('commons.home-header')
+            @yield('content')
+            @include('commons.home-footer')
+        </div>
+</html>
---
+++ app/views/school-calendar.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.home-master')
+
+@section('content')
+	<div id="content">
+    <div>
+      <h3>School Calendar (2013-2014)</h3>
+      <div class="calendar">
+        <dl>
+          <dt>MAY 2013</dt>
+          <dd>1 (Wednesday) - Labor Day (Regular Holiday)</dd>
+          <dd>2-3 (Thu-Fri) | 6-10 (Mon-Fri) - Registration Period, First Semester, AY 2013-2014 (Regular & HSU Old Students)</dd>
+          <dd>11/18/25 (Saturdays) - Registration Period, First Semester, AY 2013-2014 (CT and Graduate Programs)</dd>
+          <dd>13-17 (Mon-Fri) | 20-24 (Mon-Fri) - Registration Period, First Semester, AY 2013-2014 (New HSU Students/ Transferees)</dd>
+          <dd>27-29 (Mon-Wed) - Encoding of Summer 2013 Final Grades</dd>
+		  <dd>230-31 (Thu-Fri) - Issuance of Summer 2013 Report of Grades</dd>
+        </dl>
+        <dl>
+          <dt>JUNE 2013</dt>
+          <dd>3-7 (Mon-Fri) - Registration Period, First Semester, AY 2013-2014 for those who took the 2013 SUMMER Classes</dd>
+          <dd>10 (Monday) - First Day of Classes for the First Semester, AY 2013-2014</dd>
+          <dd>10-11 (Mon-Tue) | 13-14 (Thu-Fri) - Change of Matriculation Period</dd>
+          <dd>12 (Wednesday) - Independence Day (Regular Holiday)</dd>
+          
+        </dl>
+        <dl>
+          <dt>JULY 2013</dt>
+          <dd>1 (Monday) - Last Day for the Submission of Teachers’ & Class Programs to the Office of the Registrar</dd>
+          <dd>5 (Friday) - University Council Meeting</dd>
+          <dd>17 (Wednesday) - Welcome Ceremony for HSU students</dd>
+          <dd>26 (Friday) - Students’ Convocation</dd>
+          
+        </dl>
+        <dl>
+          <dt>AUGUST 2013</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+        <dl>
+          <dt>SEPTEMBER 2013</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+        <dl>
+          <dt>OCTOBER 2013</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+        <dl>
+          <dt>NOVEMBER 2013</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+        <dl>
+          <dt>DECEMBER 2013</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+        <dl>
+          <dt>JANUARY 2014</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+        <dl>
+          <dt>FEBRUARY 2014</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+        <dl>
+          <dt>MARCH 2014</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+        <dl>
+          <dt>DECEMBER</dt>
+          <dd>Suspendisse nulla - Sat, 5</dd>
+          <dd>Lorem tincidunt - Mon, 8</dd>
+          <dd>Malesuada sit amet - Mon, 21</dd>
+          <dd>varius non magna - Wed, 27</dd>
+          <dd>Felis et ante viverra - Thur, 28</dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+@stop
