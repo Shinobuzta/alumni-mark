ba98e4d4764b76214ede94c8b7a893fde35fb77e
Rowell Canlapan <rowell.dcanlapan@gmail.com>
2014-02-01 12:04:12 +0800

    commit again

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -53,8 +53,16 @@ class HomeController extends BaseController {
 
 	public function getLogin()
 	{
+<<<<<<< HEAD
+		return View::make("login")
+			->with('title', 'Login');
+	}	
+
+
+=======
 		return View::make("contacts")
 			->with('title', 'Login');
 	}	
+>>>>>>> e0202184eb6063388a9e3c7a88a175528609a705
 
 }
